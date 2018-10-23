7d62b787782e84e22dd43105ad09377fa8f56ae0
avh <anvesh.durgam@g.com>
2018-10-23 11:48:12 +0530

    someupdates

--- src/test/java/justtests/ExcelTest.java
+++ src/test/java/justtests/ExcelTest.java
@@ -1,5 +1,13 @@
 package justtests;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
 import com.excel.utility.Xls_Reader;
 
 public class ExcelTest {
@@ -15,9 +23,25 @@ public class ExcelTest {
 
 			String firstname = excel.getCellData("signup", "Firstname", rowNum);
 			System.out.println("This is second name  =" + firstname);
-			
 
 		}
+
+	}
+
+	@Test
+	public void testdata() throws Exception {
+
+		FileInputStream files = new FileInputStream(
+				"C:\\Users\\anvesh.durgam\\git\\AutoPractice\\src\\main\\java\\com\\testdata\\Test.xlsx");
+
+		XSSFWorkbook book = new XSSFWorkbook(files);
+		System.out.println("this is sheet" + book.getSheet("signup"));
+		XSSFSheet sheet = book.getSheet("signup");
+		System.out.println("Data in sheet  " + sheet.getRow(1).getCell(2));
+		System.out.println("This is size    =" + sheet.getLastRowNum());
+		System.out.println("first name  "+sheet.getRow(1).getCell(0));
+		System.out.println("lastname  "+sheet.getRow(1).getCell(1));
+		
 
 	}
 
