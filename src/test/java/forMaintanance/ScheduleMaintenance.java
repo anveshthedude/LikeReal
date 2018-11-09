4fae8ba1ea3904ad53cf58ec7377a70f58f88459
avh <anvesh.durgam@g.com>
2018-11-09 10:36:13 +0530

    added Scheduled maint class

---
+++ src/test/java/forMaintanance/ScheduleMaintenance.java
@@ -0,0 +1,79 @@
+package forMaintanance;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ScheduleMaintenance {
+	FileInputStream files;
+
+	@Test
+	public void test() throws IOException, InterruptedException {
+
+		files = new FileInputStream(
+				"C:\\Users\\anvesh.durgam\\git\\LikeRealP\\src\\main\\java\\com\\testdata\\Test.xlsx");
+
+		XSSFWorkbook book = new XSSFWorkbook(files);
+		System.out.println("this is sheet" + book.getSheet("signup"));
+		XSSFSheet sheet = book.getSheet("signup");
+		sheet.getLastRowNum();
+
+		for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+
+			String firstname = sheet.getRow(i).getCell(0).getStringCellValue();
+
+			System.setProperty("webdriver.chrome.driver",
+					"D:\\Softwares\\Chromedriver\\chromedriver_win32\\chromedriver.exe");
+			ChromeDriver driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+			driver.get("https://portal.appointment-plus.com/ap/ap_control_v2/account_search.php");
+			driver.findElement(By.xpath("//input[@name='login']")).clear();
+			driver.findElement(By.xpath("//input[@name='login']")).sendKeys("adurge@appointmentplus.com");
+			driver.findElement(By.xpath("//input[@name='password']")).clear();
+			driver.findElement(By.xpath("//input[@name='password']")).sendKeys("m54D_QFM");
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			driver.findElement(By.xpath("//input[@id='account_number']")).clear();
+			driver.findElement(By.xpath("//input[@id='account_number']")).sendKeys(firstname);
+
+			String button1 = "//*[@id=\"loginform-appointplus";
+
+			String button2 = "-0\"]/input[5]";
+
+			driver.findElement(By.xpath("//input[@id='search']")).click();
+			driver.findElement(By.xpath(button1 + firstname + button2)).click();
+
+			Set<String> windows = driver.getWindowHandles();
+
+			Iterator<String> getwindow = windows.iterator();
+			String portal = getwindow.next();
+			String siteadmin = getwindow.next();
+
+			driver.switchTo().window(siteadmin);
+
+			driver.switchTo().frame("header");
+			driver.manage().window().maximize();
+			Thread.sleep(3000);
+			boolean Repotabvisible = driver.findElement(By.xpath("//a[@id='fl-navtab-item-reports-id']")).isDisplayed();
+			if (Repotabvisible == true)
+
+			{
+				System.out.println("AP"+firstname + "  Test is Pass");
+			} else {
+				System.out.println("AP"+firstname + "  Test is Fail");
+			}
+
+			driver.quit();
+
+		}
+
+	}
+}
---
+++ src/test/java/justtests/Bootstrap.java
@@ -0,0 +1,62 @@
+package justtests;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+final public class Bootstrap{
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		Bootstrap.boot();
+
+	}
+
+	public static void boot() {
+
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Softwares\\Chromedriver\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		driver.get("https://account.appointment-plus.com/ap/ap_admin_v2/login.php?_ga="
+				+ "2.52921185.812422054.1533807988-1345006141.1533702908");
+
+		driver.findElement(By.id("login")).sendKeys("justlikethat");
+
+		driver.findElement(By.id("password")).sendKeys("justlikethat");
+
+		driver.findElement(By.className("btn-form")).click();
+
+		driver.switchTo().frame("header");
+		driver.findElement(By.xpath("//a[@id='fl-navtab-item-reports-id']")).click();
+
+		driver.switchTo().defaultContent();
+
+		driver.switchTo().frame("sidenav");
+		driver.findElement(By.xpath("//a[@id='fl-sidenav-emailreport-id']")).click();
+		driver.switchTo().defaultContent();
+		driver.switchTo().frame("slots");
+		driver.findElement(By.xpath("//span[contains(text(),'Select Email type')]")).click();
+
+		List<WebElement> lis = driver
+				.findElements(By.xpath("//ul[@class='ui-multiselect-checkboxes ui-helper-reset']//li//label"));
+		System.out.println("This is Final count  " + lis.size());
+
+		/*
+		 * driver.switchTo().frame("slots");
+		 * driver.findElement(By.xpath("//button[@type='button']")).click();
+		 * 
+		 * List<WebElement> lis = driver .findElements(By.xpath(
+		 * "//ul[@class='ui-multiselect-checkboxes ui-helper-reset']//li//label"
+		 * ));
+		 * 
+		 * System.out.println(lis.size());
+		 */
+
+	}
+
+}
