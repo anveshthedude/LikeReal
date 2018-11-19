ee15fd66b27e5b641ccf17ee9e8f8325d63e8ebb
avh <anvesh.durgam@g.com>
2018-11-19 21:33:36 +0530

    if there are any....

--- src/test/java/justtests/IframeTest.java
+++ src/test/java/justtests/IframeTest.java
@@ -5,16 +5,19 @@ import java.io.IOException;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import com.sa.pages.LoginPage;
+
 import testbasepackage.TestBase;
 
 public class IframeTest extends TestBase {
+	LoginPage loginpage;
 
 	public IframeTest() throws IOException {
 		super();
 
 	}
 
-	@Test
+	// @Test
 	public void iframe() {
 		initizlization();
 		driver.switchTo().frame("blockrandom");
@@ -24,4 +27,15 @@ public class IframeTest extends TestBase {
 
 	}
 
+	@Test
+	public void iFrame() throws Exception {
+		initizlization();
+		loginpage = new LoginPage();
+		loginpage.ValidateloginPagetitle();
+		int frames = driver.findElements(By.tagName("frame")).size();
+		System.out.println(frames);
+
+
+
+	}
 }
