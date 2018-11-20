b58d67007ae857ad8d0c5cedfed805a9fdf0f2e7
avh <anvesh.durgam@g.com>
2018-11-20 17:57:11 +0530

    String Stirng...

--- src/test/java/justtests/StringClass.java
+++ src/test/java/justtests/StringClass.java
@@ -3,15 +3,6 @@ package justtests;
 public class StringClass {
 
 	
-	public void test(){
-		
-		
-		int a = 20;
-		
-	String b=	String.valueOf(a);
-		
-		
-		
-		
-	}
+	
+	
 }
--- src/test/java/justtests/StringTest.java
+++ src/test/java/justtests/StringTest.java
@@ -1,5 +1,8 @@
 package justtests;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class StringTest {
 
 	public static void main(String[] args) {
@@ -13,8 +16,41 @@ public class StringTest {
 		String d = String.valueOf(c);
 		System.out.println(d);// String test
 
-		
-		
+		// String ab = new String("calling");
+		String ab = "calling";
+
+		// String lesss = new String();
+		// String lesss = "";
+
+		Set<Character> sor = new LinkedHashSet<>();
+		StringBuffer and = new StringBuffer();
+
+		for (int i = 0; i < ab.length(); i++) {
+			char cha = ab.charAt(i);
+			if (!sor.contains(cha)) {
+				sor.add(cha);
+				and.append(cha);
+			}
+		}
+
+		System.out.println("result     " + and.toString());
+
+		char ch = ab.charAt(4);
+		System.out.println("second " + ch);
+
+		String u = "123456789";
+		StringBuffer bu = new StringBuffer(u);
+
+		System.out.println("reverse " + bu.reverse());
+
+		int h = u.length();
+		String re = "";
+
+		for (int i = h - 1; i >= 0; i--) {
+			re = re + u.charAt(i);
+		}
+		System.out.println("reverse 2    " + re);
+
 	}
 
 }
