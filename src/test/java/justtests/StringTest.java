b6c89965b61d6eb1272d122dd07c82e449f97b12
Anvesh <anveshdurgam6143@gmail.com>
2018-12-24 13:15:57 +0530

    updates in String Class

--- src/test/java/justtests/StringTest.java
+++ src/test/java/justtests/StringTest.java
@@ -1,17 +1,28 @@
 package justtests;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.LinkedHashSet;
+import java.util.Properties;
 import java.util.Set;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class StringTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		stre();
-		String a = "12341";
+		String a = "12345";
+
+		String avh = "abc123abc8464AFKJAKJDFOI9VAEW";
+
+		avh = avh.replace("8464", "8888"); // To Replace data from a String like
+											// 8464 to 8888
+		// avh= avh.replaceAll("[^A-Z]", ""); // to Get only capital A-Z, if you need only caps letters from String then [^A-Z].
 		
-		String avh ="abc123abc8464";
-		avh= avh.replace("8464", "");
-		System.out.println("replace all "+avh);
+		System.out.println("replace all " + avh);
 
 		int b = Integer.valueOf(a);
 		System.out.println(b);
@@ -45,24 +56,43 @@ public class StringTest {
 		System.out.println("second " + ch);
 
 		String u = "123456789";
-		StringBuffer bu = new StringBuffer(u);
+		StringBuffer bu = new StringBuffer(u); // StringBuffer class to reverse
+												// a String value
 
-		System.out.println("reverse " + bu.reverse());
+		StringBuffer ii = new StringBuffer(); // Create object of StringBuffer
+												// class
+		ii.append(u); // Add a String by using append Mehtod
+		System.out.println("let's see    " + ii.reverse()); // reverse String value by using Reverse method and print it. :)
+															 																										 
 
 		int h = u.length();
 		String re = "";
 
 		for (int i = h - 1; i >= 0; i--) {
-			re = re + u.charAt(i);
+			re = re + u.charAt(i); // it wil iterate values from back due to i--  the String value would be like {re+ u} means 9+8 then 98+7 etc.
+									
+									 
 		}
 		System.out.println("reverse 2    " + re);
 
 	}
 
-	public static void stre() {
+	public static void stre() throws IOException{
 		String multi = "abc123abc123";
+
+		System.out.println(" lol " + multi.replaceFirst("abc", ""));// Replace first method will be used to replace first values from a string to somethingg.
 		System.out.println(multi.replaceAll("abc", ""));
 
+		
+		XSSFWorkbook name = new XSSFWorkbook(); // To read excel sheet. Add reference variable of fileiputstream in XssfWorkbook constructor like XSSFWorkbook(file); 
+		name.getSheet("sheet name");
+		XSSFSheet name2 = name.getSheet("sheet name");
+		name2.getLastRowNum();
+		
+		// to read a properties files 
+		Properties file = new Properties();
+		FileInputStream st = new FileInputStream("file");
+		file.load(st);
 	}
 
 }
